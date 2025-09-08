212fbc83bf1292e04b3ab04c690f96bdbe2b16c0
ewidrick <ewidrick@colgate.edu>
2025-09-08 11:09:33 -0400

    Mathmatical equation practice

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,17 @@
 print("Hello World")
+# make a variable
+a <- 45
+# make a vector
+b <- c(776,72,9003,583)
+# subtract vector
+b - a 
+# make a variable 
+c <- 4
+# divide vector by 4
+b/c + 73
+# make a variable 
+d <- 2
+# square vector
+(b^d) * 43 - 10^6
+
+
