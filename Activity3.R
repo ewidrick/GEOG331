421443e3e41645a0122040f1ba53f6ea63b5ffb8
ewidrick <ewidrick@colgate.edu>
2025-09-23 11:10:47 -0400

    Working through activity 3

--- Activity3.R
+++ Activity3.R
@@ -1,3 +1,10 @@
+#use install.packages to install lubridate
+install.packages(c("lubridate"))
+#it is helpful to comment this line after you run this line of code on the computer
+#and the package installs. You really don't want to do this over and over again.
+
+library(lubridate)
+
 #create a function. The names of the arguments for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
 assert <- function(statement,err.message){
   #if evaluates if a statement is true or false for a single item
@@ -16,3 +23,63 @@ assert(2 == 2, "error: unequal values")
 a <- c(1,2,3,4)
 b <- c(8,4,5)
 assert(length(a) == length(b), "error: unequal length")
+
+#read in the data file
+#skip the first 3 rows since there is additional column info
+#specify the the NA is designated differently
+datW <- read.csv("Z:\\ewidrick\\GitHub\\GEOG331\\Data\\bewkes\\bewkes_weather.csv",
+                 na.strings=c("#N/A"), skip=3, header=FALSE)
+#preview data
+print(datW[1,])
+
+#get sensor info from file
+# this data table will contain all relevant units
+sensorInfo <-   read.csv("Z:\\ewidrick\\GitHub\\GEOG331\\Data\\bewkes\\bewkes_weather.csv",
+                         na.strings=c("#N/A"), nrows=2)
+
+print(sensorInfo)
+
+#get column names from sensorInfo table
+# and set weather station colnames  to be the same
+colnames(datW) <-   colnames(sensorInfo)
+#preview data
+print(datW[1,])
+
+#convert to standardized format
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calculations
+datW[1,]
+
+#see how many values have missing data for each sensor observation
+#air temperature
+length(which(is.na(datW$air.temperature)))
+
+#wind speed
+length(which(is.na(datW$wind.speed)))
+
+#precipitation
+length(which(is.na(datW$precipitation)))
+
+#soil temperature
+length(which(is.na(datW$soil.moisture)))
+
+#soil moisture
+length(which(is.na(datW$soil.temp)))
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
