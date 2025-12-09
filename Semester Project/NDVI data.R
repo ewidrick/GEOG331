00c437353b4c8be7961aa367096ea3591d94aa4b
ewidrick <ewidrick@colgate.edu>
2025-12-09 00:00:56 -0500

    Done with figures

--- Semester Project/NDVI data.R
+++ Semester Project/NDVI data.R
@@ -13,11 +13,11 @@ first
 lc1 <- rast(first[5:13])
 
 # calculate ndvi
-ndvi1 <- (lc[[5]]-lc[[4]])/(lc[[5]]+lc[[4]])
+ndvi1 <- (lc1[[5]]-lc1[[4]])/(lc1[[5]]+lc1[[4]])
 
-names(ndvi) <- "NDVI"
+names(ndvi1) <- "NDVI"
 # create a quick plot to see how the data looks
-plot(ndvi)
+plot(ndvi1)
 
 #September 11th 2020
 #load in files
@@ -29,8 +29,24 @@ Sec
 lc2 <- rast(Sec[5:13])
 
 # calculate ndvi
-ndvi2 <- (lc[[5]]-lc[[4]])/(lc[[5]]+lc[[4]])
+ndvi2 <- (lc2[[5]]-lc2[[4]])/(lc2[[5]]+lc2[[4]])
 
-names(ndvi) <- "NDVI"
+names(ndvi2) <- "NDVI"
 # create a quick plot to see how the data looks
-plot(ndvi)
+plot(ndvi2)
+
+#October 3rd 2020
+#load in files
+third <- list.files("Z:/ewidrick/Data/045030/LC08_L2SP_045030_20201003_20201015_02_T1", full.names = T)
+
+third
+
+# read in files 3-10 as a single multi-band raster
+lc3 <- rast(third[5:13])
+
+# calculate ndvi
+ndvi3 <- (lc3[[5]]-lc3[[4]])/(lc3[[5]]+lc3[[4]])
+
+names(ndvi3) <- "NDVI"
+# create a quick plot to see how the data looks
+plot(ndvi3)
